535b73d6e459b83b0a4f80517bd0b5f3c71601e6
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-10-18 16:54:40 +0900

    fix lintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,6 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  ignorePatterns: ["webpack/"],
+  ignorePatterns: ["webpack/", "/plugins"],
   rules: {},
 }
