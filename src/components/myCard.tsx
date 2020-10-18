e970400cc5f250a47f8859ddc0e340f0a03008f1
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-10-18 17:22:24 +0900

    fix

--- src/components/myCard.tsx
+++ src/components/myCard.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 import Grid from "@material-ui/core/Grid"
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles"
 import Card from "@material-ui/core/Card"
 import CardActionArea from "@material-ui/core/CardActionArea"
 import CardContent from "@material-ui/core/CardContent"
 import Typography from "@material-ui/core/Typography"
 import Add from "@material-ui/icons/Add"
+import theme from "../theme"
 
 const useStyles = makeStyles({
   root: {
@@ -27,21 +28,23 @@ export const MyCard: React.FC = () => {
 
   return (
     <Link to="/addition/" style={{ textDecoration: "none" }}>
-      <Card style={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <Grid item className={classes.media}>
-            <Add style={{ width: 100, height: 100, color: "#1976D2" }} />
-          </Grid>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              たしざん
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              ２つの数のたしざんをします。
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <ThemeProvider theme={theme}>
+        <Card style={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <Grid item className={classes.media}>
+              <Add style={{ width: 100, height: 100, color: "#1976D2" }} />
+            </Grid>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                たしざん
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                ２つの数のたしざんをします。
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </ThemeProvider>
     </Link>
   )
 }
