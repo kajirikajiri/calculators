ba872c5fbd292086b0954b6c237684e66565c3ce
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-10-18 17:28:18 +0900

    fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,6 +10,8 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import { Header } from "./header"
 import "./layout.css"
+import { ThemeProvider } from "@material-ui/core"
+import theme from "../theme"
 
 type LayoutProps = {
   children: JSX.Element[]
@@ -28,25 +30,27 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
+      <ThemeProvider theme={theme}>
+        <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+        <div
           style={{
-            marginTop: `2rem`,
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0 1.0875rem 1.45rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://github.com/kajirikajiri">@kajirikajiri</a>
-        </footer>
-      </div>
+          <main>{children}</main>
+          <footer
+            style={{
+              marginTop: `2rem`,
+            }}
+          >
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://github.com/kajirikajiri">@kajirikajiri</a>
+          </footer>
+        </div>
+      </ThemeProvider>
     </>
   )
 }
--- src/components/myCard.tsx
+++ src/components/myCard.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 import Grid from "@material-ui/core/Grid"
-import { makeStyles, ThemeProvider } from "@material-ui/core/styles"
+import { makeStyles } from "@material-ui/core/styles"
 import Card from "@material-ui/core/Card"
 import CardActionArea from "@material-ui/core/CardActionArea"
 import CardContent from "@material-ui/core/CardContent"
 import Typography from "@material-ui/core/Typography"
 import Add from "@material-ui/icons/Add"
-import theme from "../theme"
+import styled from "styled-components"
 
 const useStyles = makeStyles({
   root: {
@@ -28,23 +28,21 @@ export const MyCard: React.FC = () => {
 
   return (
     <Link to="/addition/" style={{ textDecoration: "none" }}>
-      <ThemeProvider theme={theme}>
-        <Card style={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <Grid item className={classes.media}>
-              <Add style={{ width: 100, height: 100, color: "#1976D2" }} />
-            </Grid>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                たしざん
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                ２つの数のたしざんをします。
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </ThemeProvider>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <Grid item className={classes.media}>
+            <Add style={{ width: 100, height: 100, color: "#1976D2" }} />
+          </Grid>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              たしざん
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              ２つの数のたしざんをします。
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </Link>
   )
 }
