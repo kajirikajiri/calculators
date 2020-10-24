fb3ba1c6c1f28c89f67df99664eedc638b62b17e
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-10-24 21:38:49 +0900

    fix

--- src/molecules/CardList.tsx
+++ src/molecules/CardList.tsx
@@ -1,59 +1,14 @@
-import React, { useState, useEffect } from "react"
+import React, { ReactElement } from "react"
 import Grid from "@material-ui/core/Grid"
-import AdditionCard from "./AdditionCard"
-import SubtractionCard from "./SubtractionCard"
-import MultiplicationCard from "./MultiplicationCard"
-import IpAddressCard from "./IpAddressCard"
-import { iconColors } from "../scripts/iconColors"
-// import { cards } from "../scripts/cards"
 
 type CardListProps = {
-  searchWord: string;
+  cards: { card: ReactElement<any, any> | null }[];
 }
 
-const CardList: React.FC<CardListProps> = ({ searchWord }) => {
-  const cards = [
-    {
-      card: AdditionCard,
-      tags: ["足し算", "たしざん", "たす"],
-    },
-    {
-      card: SubtractionCard,
-      tags: ["引き算", "ひきざん", "ひく"],
-    },
-    {
-      card: MultiplicationCard,
-      tags: ["掛け算", "かけざん", "かける"],
-    },
-    {
-      card: IpAddressCard,
-      tags: ["IP", "ip"],
-    },
-  ].map((obj, i) => {
-    const n = (i + 1) % 17
-    return { card: obj.card({ colorCode: iconColors[n] }), tags: obj.tags }
-  })
-
-  const [render, setRender] = useState(cards)
-
-  useEffect(() => {
-    let set = cards
-    if (searchWord !== "") {
-      set = cards.filter(obj => {
-        // 高速検索
-        // return obj.tags.includes(searchWord)
-        // 完全検索
-        return obj.tags.some(tag => {
-          return tag.indexOf(searchWord) > -1
-        })
-      })
-    }
-    setRender(set)
-  }, [searchWord])
-
+const CardList: React.FC<CardListProps> = ({ cards }) => {
   return (
     <Grid container justify={"flex-start"} spacing={3}>
-      {render.map(obj => (
+      {cards.map(obj => (
         <>{obj.card}</>
       ))}
     </Grid>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,64 @@
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useCallback, useState } from "react"
 import { Layout } from "../components/Layout"
 import { SEO } from "../components/Seo"
 import CardList from "../molecules/CardList"
 import TextField from "@material-ui/core/TextField"
+import AdditionCard from "../molecules/AdditionCard"
+import SubtractionCard from "../molecules/SubtractionCard"
+import MultiplicationCard from "../molecules/MultiplicationCard"
+import IpAddressCard from "../molecules/IpAddressCard"
+import { iconColors } from "../scripts/iconColors"
 
 const IndexPage: React.FC = () => {
-  const [searchWord, setSearchWord] = useState("")
+  const cards = [
+    {
+      card: AdditionCard,
+      tags: ["足し算", "たしざん", "たす"],
+    },
+    {
+      card: SubtractionCard,
+      tags: ["引き算", "ひきざん", "ひく"],
+    },
+    {
+      card: MultiplicationCard,
+      tags: ["掛け算", "かけざん", "かける"],
+    },
+    {
+      card: IpAddressCard,
+      tags: ["IP", "ip"],
+    },
+  ].map((obj, i) => {
+    const n = (i + 1) % 17
+    return { card: obj.card({ colorCode: iconColors[n] }), tags: obj.tags }
+  })
+
+  const [render, setRender] = useState(cards)
 
   const handler = useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-      setSearchWord(e.target.value)
+      let set = cards
+      const searchWord = e.target.value
+      if (searchWord !== "") {
+        set = cards.filter(obj => {
+          return obj.tags.some(tag => {
+            return tag.indexOf(searchWord) > -1
+          })
+        })
+      }
+      setRender(set)
     },
-    [searchWord]
+    []
   )
 
   return (
     <Layout>
       <SEO title="Home" />
-      <CardList searchWord={searchWord} />
       <TextField
         placeholder={"けんさく"}
         variant="outlined"
-        value={searchWord}
         onChange={handler}
       />
+      <CardList cards={render} />
     </Layout>
   )
 }
